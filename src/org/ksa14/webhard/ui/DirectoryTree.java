b09944ca0d407c52ea3a4d005dac39a3e98f690d
Jong Wook Kim <ilikekjw@gmail.com>
2011-09-23 20:19:55 -0400

    enabled incremental directory traversal

--- src/org/ksa14/webhard/ui/DirectoryTree.java
+++ src/org/ksa14/webhard/ui/DirectoryTree.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.tree.*;
+import javax.swing.event.*;
 
 import org.ksa14.webhard.sftp.*;
 
@@ -14,7 +15,7 @@ import org.ksa14.webhard.sftp.*;
  * 
  * @author Jongwook
  */
-public class DirectoryTree extends JTree {
+public class DirectoryTree extends JTree implements TreeSelectionListener {
 	public static final long serialVersionUID = 0L;
 	
 	DefaultMutableTreeNode top;
@@ -30,14 +31,43 @@ public class DirectoryTree extends JTree {
 		
 		this.expandRow(0);
 		this.setScrollsOnExpand(true);
+		this.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		this.addTreeSelectionListener(this);
 	}
 	
 	private void UpdateTree(DefaultMutableTreeNode parent, String path) {
 		Vector<String> dirlist = SftpUtil.GetDirectoryList(path);
 		for (int i=0; i<dirlist.size(); i++) {
+			String dir = dirlist.elementAt(i);
+			
+			// skip hidden files
+			if(dir.charAt(0) == '.') continue;
+			
 			DefaultMutableTreeNode child = new DefaultMutableTreeNode(dirlist.elementAt(i));
-			UpdateTree(child, path + "/" + dirlist.elementAt(i));
+			
+			// save the directory path to the object
+			//child.setUserObject(path + "/" + dirlist.elementAt(i));
+			
+			// create dummy child
+			child.add(new DefaultMutableTreeNode("..."));
+			
 			parent.add(child);
+		}
+	}
+	
+	public void valueChanged(TreeSelectionEvent e) {
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode)getLastSelectedPathComponent();
+		if(node == null) return;
+		
+		Object paths[] = node.getUserObjectPath();
+		StringBuffer path = new StringBuffer();
+		for(int depth = 1; depth < paths.length; ++depth) {
+			path.append("/" + paths[depth]);
+		}
+		
+		if(!node.isLeaf() && node.getChildAt(0).toString().equals("...")) {
+			node.remove(0);
+			UpdateTree(node,path.toString());
 		}
 	}
 }
